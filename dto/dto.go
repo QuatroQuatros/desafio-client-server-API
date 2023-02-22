9b157cce3b56eb8d22a85bfa0d8b822feeb0f458
QuatroQuatros <guicossalter@gmail.com>
2023-02-22 17:55:24 -0300

    [FIX] context database

---
+++ dto/dto.go
@@ -0,0 +1,5 @@
+package dto
+
+type GetCotacao struct {
+	Dolar string `json:"dolar"`
+}
--- server/main.go
+++ server/main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"time"
 
@@ -33,23 +31,7 @@ type Cotacao struct {
 	gorm.Model
 }
 
-// type Cotacao struct {
-// 	Code       string `json:"code"`
-// 	Codein     string `json:"codein"`
-// 	Name       string `json:"name"`
-// 	High       string `json:"high"`
-// 	Low        string `json:"low"`
-// 	VarBid     string `json:"varBid"`
-// 	PctChange  string `json:"pctChange"`
-// 	Bid        string `json:"bid"`
-// 	Ask        string `json:"ask"`
-// 	Timestamp  string `json:"timestamp"`
-// 	CreateDate string `json:"create_date"`
-// 	gorm.Model
-// }
-
 var db *gorm.DB
-var c *Cotacao
 
 func main() {
 	//criando o banco
@@ -75,78 +57,70 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//ctx := r.Context()
 	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
 	defer cancel()
-	log.Println("Request iniciada")
-	defer log.Println("Request finalizada")
 
-	select {
-	case <-time.After(time.Millisecond * 200):
-		cotacao, error := getCotacao()
-		if error != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		c = cotacao
-
-		data := map[string]interface{}{
-			"dolar": c.Bid,
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(data)
-
-	case <-ctx.Done():
-		http.Error(w, "Request cancelada pelo cliente", http.StatusRequestTimeout)
-	}
-
-	err := insert_cotacao(c)
+	resp, err := getCotacao(ctx)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode("err")
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	var c Cotacao
+
+	err = json.Unmarshal(body, &c)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	data := map[string]interface{}{
+		"dolar": c.Bid,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(data)
+
+	err = insert_cotacao(&c)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(err)
 		return
 	}
 
 }
 
-func getCotacao() (*Cotacao, error) {
-	resp, error := http.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
-	if error != nil {
-		return nil, error
+func getCotacao(ctx context.Context) (*http.Response, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
+	if err != nil {
+		return nil, err
 	}
 
-	defer resp.Body.Close()
-	body, error := ioutil.ReadAll(resp.Body)
-	if error != nil {
-		return nil, error
+	client := &http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		return nil, err
 	}
 
-	var c Cotacao
+	return response, nil
 
-	error = json.Unmarshal(body, &c)
-	if error != nil {
-		return nil, error
-	}
-
-	return &c, nil
 }
 
 func insert_cotacao(cotacao *Cotacao) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
 	defer cancel()
 
-	tx := db.WithContext(ctx)
+	return db.WithContext(ctx).Create(&cotacao).Error
 
-	select {
-	case <-time.After(time.Millisecond * 10):
-
-		tx.Create(&cotacao)
-
-	case <-ctx.Done():
-
-		return errors.New("Request timeout")
-	}
-	return nil
 }
